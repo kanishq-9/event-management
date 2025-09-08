d6be079ba1bd1697896ecbf23f7779b876e78d13
kanishq-9 <purohitkanishq9@gmail.com>
2025-09-08 12:35:42 +0530

    added path

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const cors = require('cors');
 const helmet = require('helmet');
 const {routeAuthHandler} = require('./routes/auth.route');
